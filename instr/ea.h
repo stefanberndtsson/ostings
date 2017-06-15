0eace6559b06e0c2e42ad0adade18dc9481b60d2
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2017-06-16 00:14:03 +0200

    Have a macro for high EA parts, to reduce screwups of masks

--- instr/ea.h
+++ instr/ea.h
@@ -5,6 +5,8 @@
 
 #define EA_MODE(op) ((op&0x38)>>3)
 #define EA_REG(op) (op&0x7)
+#define EA_HIGH_REG(op) ((op&0xe00)>>9)
+#define EA_HIGH_MODE(op) ((op&0x1c0)>>6)
 
 void ea_read_abs_long(struct instr *, LONG, LONG, enum instr_sizes);
 void ea_read_immediate(struct instr *, LONG, enum instr_sizes);
--- mnemonics/ea.h
+++ mnemonics/ea.h
@@ -5,6 +5,8 @@
 
 #define EA_MODE(op) ((op&0x38)>>3)
 #define EA_REG(op) (op&0x7)
+#define EA_HIGH_REG(op) ((op&0xe00)>>9)
+#define EA_HIGH_MODE(op) ((op&0x1c0)>>6)
 
 char *mnemonics_ea(struct cpu *, LONG addr, enum instr_sizes, int, int, int);
 
