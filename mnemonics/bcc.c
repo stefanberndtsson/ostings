600ae8b8b68ad6b8a6d794469be7ea8255ac38ec
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2017-06-16 23:10:51 +0200

    Implement Bcc with WORD sized jumps too

--- mnemonics/bcc.c
+++ mnemonics/bcc.c
@@ -8,7 +8,7 @@
 #define SHORT_OP 0x6000
 #define SHORT_OP_MASK 0xf000
 #define LONG_OP 0x6000
-#define LONG_OP_MASK 0xff00
+#define LONG_OP_MASK 0xf0ff
 
 static char *bcc_comparison_codes[16] = {
   "BRA", "BSR", "BHI", "BLS", "BCC", "BCS", "BNE", "BEQ",
@@ -42,7 +42,7 @@ static char *mnemonics_bcc_long(struct cpu *cpu, LONG addr) {
 
   op = mmu_peek_word(cpu->mmu, addr);
   code = (op&0xf00)>>8;
-  jmp_distance = SIGN_EXT_BYTE(mmu_peek_word(cpu->mmu, addr+2));
+  jmp_distance = SIGN_EXT_WORD(mmu_peek_word(cpu->mmu, addr+2));
   destination = addr+jmp_distance+2;
   
   mnemonic = ostis_alloc(15);
@@ -52,9 +52,6 @@ static char *mnemonics_bcc_long(struct cpu *cpu, LONG addr) {
 }
 
 void mnemonics_bcc_setup(struct cpu *cpu) {
-  printf("DEBUG: Before: %ld\n", (uintptr_t)cpu->internal->mnemonics[0x660a]);
   mnemonics_register(cpu, SHORT_OP, SHORT_OP_MASK, mnemonics_bcc_short);
-  printf("DEBUG: Middle: %ld\n", (uintptr_t)cpu->internal->mnemonics[0x660a]);
   mnemonics_register(cpu, LONG_OP, LONG_OP_MASK, mnemonics_bcc_long);
-  printf("DEBUG: After: %ld\n", (uintptr_t)cpu->internal->mnemonics[0x660a]);
 }
